19e383656a34e6f2836cd5a51655d4dc01c70226
zhoucumt <553817478@qq.com>
2021-03-30 16:43:47 +0800

    增加App.module.less

--- vite-vue/src/App.vue
+++ vite-vue/src/App.vue
@@ -2,21 +2,16 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <img alt="Vue logo" :src="logo" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
-  <div :class="$style.logo">logo</div>
+  <!-- <div :class="$style.logo">logo</div> -->
+    <div :class="classes.logo">logo</div>
 </template>
 
 <script setup>
 // 将资源引入为URL，并在模板中使用该变量
 import logo from '@/assets/logo.png';
 import HelloWorld from './components/HelloWorld.vue'
+import classes from './App.module.less'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
-
-<style module>
-.logo {
-  width: 200px;
-  height: 200px;
-}
-</style>
