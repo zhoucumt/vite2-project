4a965d38c1a58c2e1a847fec4558db7df22bf859
zhoucumt <553817478@qq.com>
2021-03-30 15:24:00 +0800

    vite.config.js里配置viteMockServe

--- vite-vue/vite.config.js
+++ vite-vue/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {resolve} from 'path';
+import {resolve} from 'path'
+import {viteMockServe} from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,5 +21,5 @@ export default defineConfig({
       }
     }
   },
-  plugins: [vue()]
+  plugins: [vue(), viteMockServe({})]
 })
