4a3d8243c0b2207b3ab0e0578c2826fa4ff558cf
zhoucumt <553817478@qq.com>
2021-03-30 17:05:35 +0800

    postcss配置

--- vite-vue/src/App.vue
+++ vite-vue/src/App.vue
@@ -3,7 +3,8 @@
   <img alt="Vue logo" :src="logo" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
   <!-- <div :class="$style.logo">logo</div> -->
-    <div :class="classes.logo">logo</div>
+  <div :class="classes.logo">logo</div>
+  <input type="text" placeholder="请输入姓名">
 </template>
 
 <script setup>
@@ -15,3 +16,9 @@ import classes from './App.module.less'
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
+
+<style scoped>
+::placeholder {
+  color: blue;
+}
+</style>
